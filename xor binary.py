b5529d4eb642f1e4ad60b5c26397c5be49e4ee90
Joshua Cochran <joshuamcochran@gmail.com>
2018-07-24 21:44:34 -0700

    Added UI Added dynamic paths

--- xor binary.py
+++ xor binary.py
@@ -1,20 +1,62 @@
+import sys
 from os import urandom
+from PyQt5.QtWidgets import QFrame, QPushButton, QLabel, QGridLayout, QApplication, QLineEdit
+from PyQt5.QtCore import pyqtSlot
 
 
-def genkey(length):
-    return urandom(length)
+class EncryptionFrame(QFrame):
+    def __init__(self):
+        super().__init__()
+
+        self.resize(400, 200)
+        self.setWindowTitle('Xor Encryptor')
+
+        inputLabel = QLabel('Input path: ')
+        self.inputEdit = QLineEdit()
+
+        outputLabel = QLabel('Output path: ')
+        self.outputEdit = QLineEdit()
+
+        keyLabel = QLabel("Key: ")
+        self.keyEdit = QLineEdit()
+        self.keyEdit.setPlaceholderText("blank for default")
+
+        self.executeButton = QPushButton("Execute")
+        self.executeButton.clicked.connect(self.on_button_click)
+
+        grid = QGridLayout()
+        grid.setSpacing(10)
+
+        grid.addWidget(inputLabel, 0, 0)
+        grid.addWidget(outputLabel, 1, 0)
+        grid.addWidget(self.inputEdit, 0, 1)
+        grid.addWidget(self.outputEdit, 1, 1)
+        grid.addWidget(keyLabel, 2, 0)
+        grid.addWidget(self.keyEdit, 2, 1)
+        grid.addWidget(self.executeButton, 3, 1)
+
+        self.setLayout(grid)
+        self.show()
+
+    def genkey(self, length):
+        return urandom(length)
+
+    def xordata(self, a, key1):
+        return bytearray([b ^ keyInstance for b, keyInstance in zip(bytearray(a), key1)])
+
+    @pyqtSlot()
+    def on_button_click(self):
+        with open(self.inputEdit.text(), 'rb') as inf:
+            data = inf.read()
+            key = self.genkey(len(data))
+            data2 = self.xordata(data, key)
+
+        with open(self.outputEdit.text(), 'wb') as outf:
+            outf.write(data2)
 
 
-def xor_data(a, key1):
-    return bytearray([b ^ keyInstance for b, keyInstance in zip(bytearray(a), key1)])
+if __name__ == '__main__':
 
-
-with open('BC.exe', 'rb') as inf:
-    data = inf.read()
-    key = genkey(len(data))
-    data2 = xor_data(data, key)
-
-with open('BC2.exe', 'wb') as outf:
-    data2 = xor_data(data2, key)
-    outf.write(data2)
-
+    app = QApplication(sys.argv)
+    frame = EncryptionFrame()
+    sys.exit(app.exec_())
