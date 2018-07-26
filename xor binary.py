93e53b98df946adf3e80f598c957353934b9b516
Joshua Cochran <joshuamcochran@gmail.com>
2018-07-26 00:12:29 -0700

    Added error handling to encryptionHandler and xordata functions

--- xor binary.py
+++ xor binary.py
@@ -1,5 +1,6 @@
-import sys, datetime
-from os import urandom, chdir, getcwd, sep, pardir, path
+import sys
+import datetime
+from os import urandom, getcwd
 from PyQt5.QtWidgets import QFrame, QPushButton, QLabel, QGridLayout, QApplication, QLineEdit, QFileDialog, QHBoxLayout
 from PyQt5.QtCore import pyqtSlot, QDir
 
@@ -16,14 +17,12 @@ class EncryptionFrame(QFrame):
 
         inputLabel = QLabel('Input path: ')
         self.inputEdit = QLineEdit()
-        self.inputEdit.setReadOnly(True)
         self.inputEdit.setPlaceholderText("Select file")
         self.inputButton = QPushButton("Select File")
         self.inputButton.clicked.connect(self.on_input_click)
 
         outputLabel = QLabel('Output path: ')
         self.outputEdit = QLineEdit()
-        self.outputEdit.setReadOnly(True)
         self.outputEdit.setPlaceholderText("Select file")
         self.outputButton = QPushButton("Select File")
         self.outputButton.clicked.connect(self.on_output_click)
@@ -73,52 +72,71 @@ class EncryptionFrame(QFrame):
     def genkey(self, length):
         return urandom(length)
 
-    def xordata(self, a, key1):
-        return bytearray([b ^ keyInstance for b, keyInstance in zip(bytearray(a), key1)])
+    def xordata(self, data, key):
+        if len(data) < len(key):
+            raise ValueError("Invalid key length. Key length must be greater than or equal to data size.")
+        try:
+            return bytearray([dataByte ^ keyByte for dataByte, keyByte in zip(bytearray(data), key)])
+        except TypeError:
+            print("Error! Invalid data or key. Try again...")
 
-    def encrypt(self):
-        if not self.randomKey and self.key == 0x00:
+    def encryptionHandler(self):
+        if not self.randomKey and self.key == bytearray(0x00):
             return
 
-        if self.inputEdit.text() != "" and self.inputEdit.text() != "Path not found":
-            with open(self.inputEdit.text(), 'rb') as inf:
+        try:
+            inf = open(self.inputEdit.text(), 'rb')
+        except IOError:
+            print("Could not read file:", self.inputEdit.text())
+            return
+        else:
+            with inf:
                 data = inf.read()
-                if self.randomKey:
-                    self.key = self.genkey(len(data))
-        else:
-            return
 
-        data2 = self.xordata(data, self.key)
-        if self.outputEdit.text() != "" and self.outputEdit.text() != "Path not found":
-            with open(self.outputEdit.text(), 'wb') as outf:
-                outf.write(data2)
-        else:
+        if self.randomKey:
+            self.key = self.genkey(len(data))
+
+        encryptedData = self.xordata(data, self.key)
+
+        try:
+            outf = open(self.outputEdit.text(), 'wb')
+        except IOError:
+            print("Could not read file:", self.outputEdit.text())
             return
+        else:
+            with outf:
+                outf.write(encryptedData)
 
         folder = getcwd()
         date = datetime.datetime.now().strftime("%Y-%m-%d %Hhr%Mmin%Ss")
         with open(folder + "/" + date + ".txt", 'wb') as outf:
             outf.write(self.key)
 
+        self.key = bytearray(0x00)
+
     @pyqtSlot()
     def on_button_click(self):
-        self.encrypt()
+        self.encryptionHandler()
 
     @pyqtSlot()
     def on_key_select_click(self):
         self.randomKey = False
         file, _filter = QFileDialog.getOpenFileName(self, "Select Key", QDir.currentPath())
 
-        if file != "":
-            with open(file, 'rb') as inf:
+        try:
+            inf = open(file, 'rb')
+        except IOError:
+            print("file not found:", file)
+        else:
+            with inf:
                 self.key = inf.read()
 
-        self.keyEdit.setPlaceholderText(file)
+        self.keyEdit.setText(file)
 
     @pyqtSlot()
     def on_random_click(self):
         self.randomKey = True
-        self.keyEdit.setPlaceholderText("Random key")
+        self.keyEdit.setText("Random key")
 
     @pyqtSlot()
     def on_input_click(self):
